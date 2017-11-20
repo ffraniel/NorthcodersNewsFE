bd3e64fba7e9b4ebc50adafdbcaea7bf9cbc42c5
ffraniel <francis.whitehead@googlemail.com>
2017-11-20 12:13:02 +0000

    styling of article comments

--- src/ArticleComments.js
+++ src/ArticleComments.js
@@ -1,48 +1,67 @@
-import React, { Component } from 'react';
-import './ArticleComments.css';
-import Search from './Search';
-import LoadingComp from './LoadingComp';
-import Comment from './Comment';
+import React, { Component } from "react";
+import "./ArticleComments.css";
+import Search from "./Search";
+import LoadingComp from "./LoadingComp";
+import Comment from "./Comment";
 
 class ArticleComments extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = ({
-            comments : this.props.comments,
-            searchVal:'',
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: this.props.comments,
+      searchVal: ""
+    };
+  }
+  // id: "5834129d5905f02e4c8e6e5d",
+  // body: "Bepiha hocrem ruc pobdekmor va zet no neomo ludafi piw vowes sevjejsec uta sukar fe ni dafal ibekal. Mo goed zo ziwelbuk kid kuddodu pilweor mu gizidvag fa rarribva decajco ser lah. Sabgu pe uto tenodsa be suisje va zed lij gikru hur ohohujuj venul inseksu zasne ifegefida we uvabako. Dogun loifwa teviba celjis ijekoji hipikav ru pobe kolzompil caroto nowakbod fimomgu lo iz zak sotadag.",
+  // belongs_to: "583412935905f02e4c8e6e06",
+  // created_by: "grumpy19",
+  // votes: 17,
+  // created_at: 1479515865000,
 
-    }
-    // id: "5834129d5905f02e4c8e6e5d",
-    // body: "Bepiha hocrem ruc pobdekmor va zet no neomo ludafi piw vowes sevjejsec uta sukar fe ni dafal ibekal. Mo goed zo ziwelbuk kid kuddodu pilweor mu gizidvag fa rarribva decajco ser lah. Sabgu pe uto tenodsa be suisje va zed lij gikru hur ohohujuj venul inseksu zasne ifegefida we uvabako. Dogun loifwa teviba celjis ijekoji hipikav ru pobe kolzompil caroto nowakbod fimomgu lo iz zak sotadag.",
-    // belongs_to: "583412935905f02e4c8e6e06",
-    // created_by: "grumpy19",
-    // votes: 17,
-    // created_at: 1479515865000,
-
-    render(){
-        return ( 
-            <div className="commentsIterator">
-
-            {this.state.comments.map((comment) => {            
-                return (
-                    <div className="commentEntry">
-                    <div className="rankAndVote">
-                        <a href="#"><p>up</p></a>
-                        <p>{comment.votes}</p>
-                        <a href="#"><p>down</p></a>
-                    </div>
-                    <div className="commentItem">
-                        <p>Posted at {comment.created_at}</p>
-                        <h2>{comment.body}</h2>
-                        <h4>{comment.created_by}</h4>
-                    </div>
-                    </div>
-               )
-              })}
+  render() {
+    return (
+      <div className="commentsIterator">
+        {this.state.comments.map(comment => {
+          return (
+            <div className="commentEntry">
+              <div className="rankAndVote">
+                <a href="#">
+                  <p>up</p>
+                </a>
+                <p>{comment.votes}</p>
+                <a href="#">
+                  <p>down</p>
+                </a>
+              </div>
+              <div className="commentItem">
+                <p>{comment.body}</p>
+                <a href="#" className="authoredBy">
+                  <h4>Submitted 6 hours ago by {comment.created_by}</h4>
+                </a>
+                <div className="listLinks">
+                  <a href="#">
+                    <p>permalink</p>
+                  </a>
+                  <a href="#">
+                    <p>embed</p>
+                  </a>
+                  <a href="#">
+                    <p>save</p>
+                  </a>
+                  <a href="#">
+                    <p>report</p>
+                  </a>
+                  <a href="#">
+                    <p>reply</p>
+                  </a>
+                </div>
+              </div>
             </div>
-        )
-    }
-
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default ArticleComments;
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,56 +1,61 @@
-import React, { Component } from 'react';
-import Listcss from './List.css';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import React, { Component } from "react";
+import Listcss from "./List.css";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 
 class ListItem extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = ({
-            posts:this.props.posts
-        })
-    }
-    render(){
-        return ( 
-            <div className="listIterator">
-            {this.state.posts.map((post, index) => {            
-                return (
-                    <div className="listEntry">
-                    <div className="rankAndVote">
-                        <h1>{index + 1}</h1>
-                        <a href="#"><p>up</p></a>
-                        <p>{post.votes}</p>
-                        <a href="#"><p>down</p></a>
-                    </div>
-                    <div className="listItem">
-                        <a href="#">
-                            <h2 className="artTitle">{post.title}</h2>
-                        </a>
-                        <p className="authoredBy">Submitted 6 hours ago by <a href="#">{post.created_by}</a></p>
-                        <div className="listLinks">
-                            <Link to={`/{post.id}/comments`}>
-                                <p>{post.comments} comments</p>
-                               
-                            </Link>
-                            <a href="#">
-                                <p>share</p>
-                            </a>
-                            <a href="#">
-                                <p>save</p>
-                            </a>
-                            <a href="#">
-                                <p>hide</p>
-                            </a>
-                            <a href="#">
-                                <p>report</p>
-                            </a>
-                        </div> 
-                    </div>
+    this.state = {
+      posts: this.props.posts
+    };
+  }
+  render() {
+    return (
+      <div className="listIterator">
+        {this.state.posts.map((post, index) => {
+          return (
+            <div className="listEntry">
+              <div className="rankAndVote">
+                <h1>{index + 1}</h1>
+                <a href="#">
+                  <p>up</p>
+                </a>
+                <p>{post.votes}</p>
+                <a href="#">
+                  <p>down</p>
+                </a>
+              </div>
+              <div className="listItem">
+                <a href="#">
+                  <h2 className="artTitle">{post.title}</h2>
+                </a>
+                <p className="authoredBy">
+                  Submitted 6 hours ago by <a href="#">{post.created_by}</a>
+                </p>
+                <div className="listLinks">
+                  <Link to={`/{post.id}/comments`}>
+                    <p>{post.comments} comments</p>
+                  </Link>
+                  <a href="#">
+                    <p>share</p>
+                  </a>
+                  <a href="#">
+                    <p>save</p>
+                  </a>
+                  <a href="#">
+                    <p>hide</p>
+                  </a>
+                  <a href="#">
+                    <p>report</p>
+                  </a>
                 </div>
-               )
-              })}
+              </div>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default ListItem;
