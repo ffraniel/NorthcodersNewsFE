19fd139a8e1118497e3a7ffd9f88e43f927f6c38
ffraniel <francis.whitehead@googlemail.com>
2018-01-03 17:16:52 +0000

    implement 768px responsive breakpoint

--- src/Search.js
+++ src/Search.js
@@ -18,7 +18,7 @@ class Search extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                         Search
                         <input className="searchInput" type="text" value={this.state.searchVal} onChange={this.handleChange} />
-                        <input type="submit" value="Submit" />
+                        <input id="submitSearch" type="submit" value="Submit" />
                     </form> 
                 </dfn>
             </div>
